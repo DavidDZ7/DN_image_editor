6984cde51d89b03c71684a8f03d8d23abdd112ac
David Norman Diaz Estrada <daviddiaz997@gmail.com>
2023-07-29 15:03:09 -0600

    Added comments and debug mode

--- source/LinearGradient.py
+++ source/LinearGradient.py
@@ -3,24 +3,30 @@ Script to compute a linear gradient between 2 colors
 David Norman Diaz Estrada - 2023
 """
 def getGradient(px1,px2):
+    """
+    Function to compute linear gradient between 2 colors
+    :param px1: color 1
+    :param px2: color 2
+    :return: returns 3 lists with R,G,B components of colors inside the gradient
+    """
     r1, g1, b1 = px1[0], px1[1], px1[2]
     r2, g2, b2 = px2[0], px2[1], px2[2]
+    #Compute modulus (magnitude) of vector between px1 and px2:
+    modulus=((r1-r2)**2+(g1-g2)**2+(b1-b2)**2)**0.5
+    #Compute components of unit vector:
+    uR=float(abs(r2-r1))/modulus
+    uG=float(abs(g2-g1))/modulus
+    uB=float(abs(b2-b1))/modulus
 
-    modulo=((r1-r2)**2+(g1-g2)**2+(b1-b2)**2)**0.5
-    #calculamos componentes del vector unitario:
-    uR=float(abs(r2-r1))/modulo
-    uG=float(abs(g2-g1))/modulo
-    uB=float(abs(b2-b1))/modulo
+    partitions=7 # Number of partitions in the gradient (number of tones-1)
+    #Initialize list to store RGB values of colors in gradient:
+    R=[0]*(partitions+1)
+    G=[0]*(partitions+1)
+    B=[0]*(partitions+1)
+    #resolution:
+    res=modulus/partitions
 
-    particiones=7#número de tonos que forman parte del gradiente
-    #inicializamos valores en cero para listas que almacenarán valores de los tonos del gradiente:
-    R=[0]*(particiones+1)
-    G=[0]*(particiones+1)
-    B=[0]*(particiones+1)
-    #resolución:
-    res=modulo/particiones
-
-    #calculamos cual es la coordenada mínima en cada eje para siempre ir aumentando de menor a mayor
+    #Identify the minimum coordinate in each axis, so we can compute gradient from smaller to greatest value:
     #rr=min(r1,r2)
     #gg=min(g1,g2)
     #bb=min(b1,b2)
@@ -31,20 +37,20 @@ def getGradient(px1,px2):
     if b1 <= b2:signB = 1
     else: signB = -1
 
-    #calculamos los tonos pertenecientes al linear gradient:
-    for n in range(0,particiones+1):
-        R[n] = r1 + signR*round(uR * (n*res))#valor inicial + (vector unitario)*(n*resolución)
-        G[n] = g1 + signG*round(uG * (n*res))#valor inicial + (vector unitario)*(n*resolución)
-        B[n] = b1 + signB*round(uB * (n*res))#valor inicial + (vector unitario)*(n*resolución)
+    #Compute tones inside the linear:
+    for n in range(0,partitions+1):
+        R[n] = r1 + signR*round(uR * (n*res))#initial value + (unit vector)*(n*resolution)
+        G[n] = g1 + signG*round(uG * (n*res))#initial value + (unit vector)*(n*resolution)
+        B[n] = b1 + signB*round(uB * (n*res))#initial value + (unit vector)*(n*resolution)
 
     """
     px1 = [r1, g1, b1]
     px2 = [r2, g2, b2]
     print("px1: " + str(px1))
     print("px2: " + str(px2))
-    print("modulo: " + str(modulo))
-    print("particiones: " + str(particiones))
-    print("resolución: " + str(res))
+    print("modulus: " + str(modulus))
+    print("partitions: " + str(partitions))
+    print("resolutions: " + str(res))
     print("R: " + str(R))
     print("G: " + str(G))
     print("B: " + str(B))
@@ -62,20 +68,22 @@ def plotGradient(R,G,B):
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
 
-    particiones=7
+    partitions=7
 
     fig = plt.figure()
     ax = Axes3D(fig)
-    for i in range(0,particiones+1):
+    for i in range(0,partitions+1):
         color=[R[i], G[i], B[i]]
         ax.scatter(R[i], G[i], B[i],  color=rgb_to_hexa(color))
     plt.show()
 
 
+if __name__ == "__main__":
+    print("-" * 20)
+    print("Debug mode")
+    print("-" * 20)
 
-"""
-px1=[250,160,70]
-px2=[100,20,30]
-R,G,B = getGradient(px1,px2)
-plotGradient(R,G,B)
-"""
+    px1 = [250, 160, 70]
+    px2 = [100, 20, 30]
+    R, G, B = getGradient(px1, px2)
+    plotGradient(R, G, B)
